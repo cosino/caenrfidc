0218af14b9b730b15aa7f6476268ebaa12947828
Rodolfo Giometti <giometti@linux.it>
2015-02-02 19:01:31 +0100

    src: add help for specific command
    Signed-off-by: Rodolfo Giometti <giometti@linux.it>

--- src/cmd_help.c
+++ src/cmd_help.c
@@ -10,9 +10,23 @@ int cmd_help(struct command_s *cmd, int argc, char *argv[])
 
 	PDEBUG("%s", __func__);
 
-	PDATA("commands list:");
+	if (argc == 1) {
+		PDATA("commands list:");
+		for (ptr = commands_list; ptr->name; ptr++)
+			PDATA("\t%s %s", ptr->name, ptr->usage ? ptr->usage : "");
+
+		return 0;
+	}
+
+	/* Display specific command help */
 	for (ptr = commands_list; ptr->name; ptr++)
-		PDATA("\t%s %s", ptr->name, ptr->usage ? ptr->usage : "");
+		if (strcmp(ptr->name, argv[1]) == 0)
+			break;
+	if (!ptr->name) {
+		PERR("unknow command %s", argv[1]);
+		return -1;
+	}
+	PDATA("%s %s", ptr->name, ptr->usage ? ptr->usage : "");
 
 	return 0;
 }
